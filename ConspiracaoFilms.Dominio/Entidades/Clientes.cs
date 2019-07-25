acd9219332f41dc6790a4509c2424e45dadd838f
Lester Cerioli <admin@lucastecnologia.onmicrosoft.com>
2019-07-25 19:57:59 -0300

    Conclusão de criação dos contratos e suas Interfaces

---
+++ ConspiracaoFilms.Dominio/Entidades/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConspiracaoFilms.Dominio.Entidades
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string CNPJ { get; set; }
+        public DateTime DataDeCadastro { get; set; }
+
+        
+    }
+}
---
+++ ConspiracaoFilms.Dominio/Entidades/Produtos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConspiracaoFilms.Dominio.Entidades
+{
+    public class Produtos : Entidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public int Quantidade { get; set; }
+        public string UnidadeMedida { get; set; }
+        public decimal Peso { get; set; }
+
+        public override void Validate()
+        {
+            if (string.IsNullOrEmpty(Nome))
+                AdicionarCritica("Nome do produto não foi informado");
+
+            if (string.IsNullOrEmpty(Tipo))
+                AdicionarCritica("Tipo deve ser preenchido");
+
+        }
+    }
+}
